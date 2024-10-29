e62a6720279e8448eeb92f9d5ba02a26ff94d96d
Saumyajeet Das <111430070+saumyajeet1822@users.noreply.github.com>
2024-10-29 11:33:54 +0530

    Create getsandPuts.c

---
+++ getsandPuts.c
@@ -0,0 +1,13 @@
+//gets or puts
+#include<stdio.h>
+int main(){
+    char name[25];
+    printf("Please enter the name ");
+    gets(name);
+    puts("Hello");
+    puts(name);
+    return 0;
+}
+
+
+
